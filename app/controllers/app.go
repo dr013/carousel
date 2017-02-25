369042881d6f0ed875fb25f1b45bdfd262ee1d15
Evgeny Krukov <krukov@bpcbt.com>
2017-02-25 11:09:12 +0300

    Clear Revel

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,25 +1,11 @@
 package controllers
 
-import (
-        "github.com/revel/revel"
-        "github.com/dr013/carousel/app/models"
-)
+import "github.com/revel/revel"
 
 type App struct {
-        *revel.Controller
+	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-        var (
-                schemas []models.Schema
-                err     error
-        )
-        schemas, err = models.GetSchemas()
-        if err != nil {
-                errResp := buildErrResponse(err, "500")
-                c.Response.Status = 500
-                return c.RenderJson(errResp)
-        }
-        c.Response.Status = 200
-        return c.Render(schemas)
+	return c.Render()
 }
